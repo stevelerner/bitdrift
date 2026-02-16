6cadcc82f98336e2af2aa6a5aff20fc8aa9b4d30
sl <sl@sls-MacBook-Air.local>
2026-02-16 13:44:32 -0800

    commit

--- sankeydemo/android/no-sdk/sankey-demo-no-sdk-w-quits/app/build.gradle.kts
+++ sankeydemo/android/no-sdk/sankey-demo-no-sdk-w-quits/app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
